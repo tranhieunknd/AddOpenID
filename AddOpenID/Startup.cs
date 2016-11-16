2235c0786b5ca83fef21f32cc1153b1fc4890545
tranh <tranh@TRAN_HIEU_NKND>
2016-11-16 17:20:53 +0700

    Add project files.

---
+++ AddOpenID/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AddOpenID.Startup))]
+namespace AddOpenID
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
